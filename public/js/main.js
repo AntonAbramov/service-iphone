5a3d89e5c330a26b678d3efb8dce3b2264932306
anton <AntonAbramov1990@gmail.com>
2015-11-01 19:24:00 +0200

    update keywords, images, refatoring index page

--- public/js/main.js
+++ public/js/main.js
@@ -3,12 +3,24 @@ requirejs([
   './config',
   './helpers/domHelper',
   './modules/navigation',
-  './modules/courier'
-], function (config, domHelper, Nav, Courier) {
+  './modules/courier',
+  './modules/lazy'
+], function (config, domHelper, Nav, Courier, Lazy) {
   'use strict';
 
 
+  /*function loadCss(url) {
+    var link = document.createElement("link");
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = url;
+    document.getElementsByTagName("head")[0].appendChild(link);
+  }*/
+
+//  loadCss('css/style.css');
   Nav.init();
+  Lazy.init();
   Courier.init();
+
 
 });
