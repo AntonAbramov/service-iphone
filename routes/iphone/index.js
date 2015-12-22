801208ed3ceefc69e062273c10ca32d2bf4c5ad3
antonabramov <anton@juniqe.com>
2015-12-22 23:39:56 +0100

    make iphone 4 articles work

--- routes/iphone/index.js
+++ routes/iphone/index.js
@@ -28,8 +28,11 @@ router.get('/remont-iphone3g', function (req, res, next) {
   });
 });
 
-router.get('/remont-iphone4', function (req, res, next) {
 
+//======================
+//    iPhone 4
+//======================
+router.get('/remont-iphone4', function (req, res, next) {
   req.db['iphone4'].find().sort({'idx' : 1}, function (err, docs) {
     if (err) throw err;
     console.log(docs);
@@ -41,9 +44,137 @@ router.get('/remont-iphone4', function (req, res, next) {
       pricesTable: docs
     });
   });
-
 });
 
+router.get('/remont-iphone4/diagnostika', function (req, res, next) {
+  res.render('pages/iphones/iphone4/diagnostika', {
+    title: "Диагностика iPhone 4 в Киеве | Service-iPhone ремонт Айфон 4",
+    description: "Бесплатная диагностика iPhone 4 в Киеве. В Service-iPhone профессионалы разберутся что случилось с Вашим Айфоном 4.",
+    keywords: "Диагностика iphone 4, Киев, Даигностика айфон 4, Киев Диагностика iPhone 4",
+    menuActive: 'iphone'
+  });
+});
+
+router.get('/remont-iphone4/remnont-knopki-home', function (req, res, next) {
+  res.render('pages/iphones/iphone4/remnont-knopki-home', {
+    title: "Ремонт кнопки Home в iPhone 4 | Service-iPhone ремонт айфон 4",
+    description: "Не работает кнопка Home на iPhone 4, мы проведем беслпатную диагностику в Киеве, и сделаем ремонт iphone 4 за 20 минут.",
+    keywords: "Ремонт кнопки Home iPhone 4, замена кнопки home iphone 4, Киев, ремонт iHome iPhone 4",
+    menuActive: 'iphone'
+  });
+});
+
+router.get('/remont-iphone4/remont-knopki-vkluchenia', function (req, res, next) {
+  res.render('pages/iphones/iphone4/remont-knopki-vkluchenia', {
+    title: "Ремонт кнопки включения (блокировки) iPhone 4",
+    description: "Когда перестала работать кнопка включения или блокирования в iphone 4, обращайтесь за бесплатной диагностикой в Service-iPhone в Киеве",
+    keywords: "Ремонт кнопки включения iPhone 4, Киев, айфон 4, ремонт кнопки блокировки iphone 4 ",
+    menuActive: 'iphone'
+  });
+});
+
+router.get('/remont-iphone4/remont-knopok-gromkosti', function (req, res, next) {
+  res.render('pages/iphones/iphone4/remont-knopok-gromkosti', {
+    title: "Ремонт кнопок громкости iPhone 4 | Service-iPhone в Киеве",
+    description: "не работают кнопки громкости в iphone 4? В Service-iPhone проведут бесплатную диагностику и скажут причину поломки за 20 минут.",
+    keywords: "Ремонт кнопки громкостки iPhone 4, не работает кнопка звука айфон 4",
+    menuActive: 'iphone'
+  });
+});
+
+router.get('/remont-iphone4/zamena-batarei', function (req, res, next) {
+  res.render('pages/iphones/iphone4/zamena-batarei', {
+    title: "Замена батареи (аккумулятора) iPhone 4 в Киеве | Service-iPhone",
+    description: "iPhone быстро разряжается? Или не работает батарея? В Service-iPhone быстро найдут причину неполадки и сделают качественный ремонт",
+    keywords: "Замена батареи iPhone 4, Киев, ремонт аккумулятора iphone 4, замена батареи айфон 4",
+    menuActive: 'iphone'
+  });
+});
+
+router.get('/remont-iphone4/zamena-dinamika', function (req, res, next) {
+  res.render('pages/iphones/iphone4/zamena-dinamika', {
+    title: "Замена полифонического динамика (buzzer) iPhone 4 | Service-iPhone",
+    description: "Если перестал работать звук на iPhone 4, лучше обратиться за консультацией к специалистам. В нашем сервисном центре сделают бесплатную диагностику.",
+    keywords: "Замена динамика iPhone 4 в Киеве, ремонт динамика iphone 4, замена динамика айфон 4",
+    menuActive: 'iphone'
+  });
+});
+
+router.get('/remont-iphone4/zamena-ekrana', function (req, res, next) {
+  res.render('pages/iphones/iphone4/zamena-ekrana', {
+    title: "Замена экрана (дисплея) iPhone 4 в Киеве | Service-iPhone",
+    description: "Качественная замена экрана (дисплея) на iPhone 4 в Киеве. Цены от 200 грн. Время на ремонт 30 минут.",
+    keywords: "Замена дисплея iphone 4, замена экрана iphone 4, Киев, замена дисплея айфон 4, экрана",
+    menuActive: 'iphone'
+  });
+});
+
+router.get('/remont-iphone4/zamena-front-camera', function (req, res, next) {
+  res.render('pages/iphones/iphone4/zamena-front-camera', {
+    title: "Замена фронтальной (передней) камеры iPhone 4 | Service-iPhone",
+    description: "Ремонт фронтальной камеры в iPhone 4 занимает от 30 минут. В Service-iPhone делает качественный ремонт в городе Киеве",
+    keywords: "Замена фронтальной камены iPhone 4, ремонт передней камеры iPhone 4, ремонт фронтальной камеры айфон 4, Киев",
+    menuActive: 'iphone'
+  });
+});
+
+router.get('/remont-iphone4/zamena-kameru', function (req, res, next) {
+  res.render('pages/iphones/iphone4/zamena-kameru', {
+    title: "Замена основной (задней) камеры iPhone 4 | Service-iPhone",
+    description: "Не работает камера? Необходимо произвести диагностику дивайся, и заменить камеру. В Service-iPhone Работаю професионалы которые могут заменить камеру за считаные минуты.",
+    keywords: "Замена основной камеры iPhone 4, ремонт камеры iPhone 4, замена камеры айфон 4, Киев",
+    menuActive: 'iphone'
+  });
+});
+
+router.get('/remont-iphone4/zamena-korpysa', function (req, res, next) {
+  res.render('pages/iphones/iphone4/zamena-korpysa', {
+    title: "Замена корпуса iPhone 4 в Киеве | Service-iPhone",
+    description: "Разбили или полнули корпус iPhone 4? Не беда, наши специлисты быстро вылечат ваш айфон за 30 минут",
+    keywords: "Замена корпуса iPhone 4, Замена корпуса айфон 4, Киев",
+    menuActive: 'iphone'
+  });
+});
+
+router.get('/remont-iphone4/zamena-microfona', function (req, res, next) {
+  res.render('pages/iphones/iphone4/zamena-microfona', {
+    title: "Замена микрофона iPhone 4 | Service-iPhone",
+    description: "Не слышит собеседник? Что делать если вас не слышно? В Service-Iphone знают как решить эту проблему.",
+    keywords: "Замена микрофона iPhone 4 в Киеве, Замена микрофона айфон 4, ремонт микрофона iphone 4",
+    menuActive: 'iphone'
+  });
+});
+
+router.get('/remont-iphone4/zamena-razgavornogo-dinamika', function (req, res, next) {
+  res.render('pages/iphones/iphone4/zamena-razgavornogo-dinamika', {
+    title: "Замена разговорного динамика iPhone 4 | Service-iPhone",
+    description: "Пропал звук? Попробуйте перезагрузить телефон, если проблема не решиться необходимо делать диагностику устройства",
+    keywords: "Замена разговорного динамика iPhone 4, Замена динамика айфон 4 в Киеве, Ремонт динамика iPhone 4",
+    menuActive: 'iphone'
+  });
+});
+
+router.get('/remont-iphone4/zamena-razjoma-dlja-naushnikov', function (req, res, next) {
+  res.render('pages/iphones/iphone4/zamena-razjoma-dlja-naushnikov', {
+    title: "Замена разъёма для наушников (аудиоджека) iPhone 4 | Service-iPhone",
+    description: "Сломался разьем для наушников? В Service-iPhone устрнят даную неприятность за считаные минуты.",
+    keywords: "Замена разъёма для наушников iPhone 4, Киев, ремонт айфон 4 разьем для наушников",
+    menuActive: 'iphone'
+  });
+});
+
+router.get('/remont-iphone4/zamena-stekla', function (req, res, next) {
+  res.render('pages/iphones/iphone4/zamena-stekla', {
+    title: "Замена стекла (тачскрина) iPhone 4 в Киеве | Service-iPhone",
+    description: "Качественная замена стекла (дисплея) на iPhone 4 в Киеве. от 200 грн. Ремонт от 20 минут.",
+    keywords: "Замена стекла iphone 4, замена экрана iphone 4, Киев, замена дисплея iPhone 4, экрана",
+    menuActive: 'iphone'
+  });
+});
+
+//======================
+//    iPhone 4s
+//======================
 router.get('/remont-iphone4s', function (req, res, next) {
   res.render('pages/iphones/iphone4s', {
     title: "Ремонт iPhone 4s",
@@ -52,6 +183,11 @@ router.get('/remont-iphone4s', function (req, res, next) {
     menuActive: 'iphone'
   });
 });
+
+//======================
+//    iPhone 5
+//======================
+
 
 router.get('/remont-iphone5', function (req, res, next) {
   res.render('pages/iphones/iphone5', {
