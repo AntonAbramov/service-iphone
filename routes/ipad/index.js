3b8c41a5cf3610eabfa0c7f93da3afa4863a7404
antonabramov <anton@juniqe.com>
2016-03-19 18:20:37 +0100

    add ipad pages

--- routes/ipad/index.js
+++ routes/ipad/index.js
@@ -10,6 +10,10 @@ router.get('/ipad-remont', function (req, res, next) {
   });
 });
 
+//======================
+//    iPad 2
+//======================
+
 router.get('/remont-ipad2', function (req, res, next) {
   req.db['ipad2'].find().sort({'idx': 1}, function (err, docs) {
     if (err) throw err;
@@ -22,6 +26,37 @@ router.get('/remont-ipad2', function (req, res, next) {
     });
   });
 });
+
+router.get('/remont-ipad2/zamena-batarei', function (req, res, next) {
+  res.render('pages/ipads/ipad2/zamena-batarei', {
+    title: "Замена батареи (аккумулятора) ipad 2 в Киеве | Service-iPhone",
+    description: "ipad быстро разряжается? Или не работает батарея? В Service-ipad быстро найдут причину неполадки и сделают качественный ремонт",
+    keywords: "Замена батареи ipad 2, Киев, ремонт аккумулятора ipad 2, замена батареи айпад 2",
+    menuActive: 'ipad'
+  });
+});
+
+router.get('/remont-ipad2/zamena-ekrana', function (req, res, next) {
+  res.render('pages/ipads/ipad2/zamena-ekrana', {
+    title: "Замена экрана (дисплея) ipad 2 в Киеве | Service-iPhone",
+    description: "Качественная замена экрана (дисплея) на ipad 2 в Киеве. Цены от 200 грн. Время на ремонт от 20 минут.",
+    keywords: "Замена дисплея ipad 2, замена экрана ipad 2, Киев, замена дисплея айпад 2, экрана",
+    menuActive: 'ipad'
+  });
+});
+
+router.get('/remont-ipad2/zamena-stekla', function (req, res, next) {
+  res.render('pages/ipads/ipad2/zamena-stekla', {
+    title: "Замена стекла (тачскрина) ipad 2 в Киеве | Service-ipad",
+    description: "Качественная замена стекла (дисплея) на ipad 2 в Киеве. от 200 грн. Ремонт от 20 минут.",
+    keywords: "Замена стекла ipad 2, замена экрана ipad 2, Киев, замена дисплея ipad 2, экрана",
+    menuActive: 'ipad'
+  });
+});
+
+//======================
+//    iPad 3
+//======================
 
 router.get('/remont-ipad3', function (req, res, next) {
   req.db['ipad3'].find().sort({'idx': 1}, function (err, docs) {
@@ -36,6 +71,37 @@ router.get('/remont-ipad3', function (req, res, next) {
   });
 });
 
+router.get('/remont-ipad3/zamena-batarei', function (req, res, next) {
+  res.render('pages/ipads/ipad3/zamena-batarei', {
+    title: "Замена батареи (аккумулятора) ipad 3 в Киеве | Service-iPhone",
+    description: "ipad быстро разряжается? Или не работает батарея? В Service-ipad быстро найдут причину неполадки и сделают качественный ремонт",
+    keywords: "Замена батареи ipad 3, Киев, ремонт аккумулятора ipad 3, замена батареи айпад 3",
+    menuActive: 'ipad'
+  });
+});
+
+router.get('/remont-ipad3/zamena-ekrana', function (req, res, next) {
+  res.render('pages/ipads/ipad3/zamena-ekrana', {
+    title: "Замена экрана (дисплея) ipad 3 в Киеве | Service-iPhone",
+    description: "Качественная замена экрана (дисплея) на ipad 3 в Киеве. Цены от 300 грн. Время на ремонт от 30 минут.",
+    keywords: "Замена дисплея ipad 3, замена экрана ipad 3, Киев, замена дисплея айпад 3, экрана",
+    menuActive: 'ipad'
+  });
+});
+
+router.get('/remont-ipad3/zamena-stekla', function (req, res, next) {
+  res.render('pages/ipads/ipad3/zamena-stekla', {
+    title: "Замена стекла (тачскрина) ipad 3 в Киеве | Service-ipad",
+    description: "Качественная замена стекла (дисплея) на ipad 3 в Киеве. от 300 грн. Ремонт от 30 минут.",
+    keywords: "Замена стекла ipad 3, замена экрана ipad 3, Киев, замена дисплея ipad 3, экрана",
+    menuActive: 'ipad'
+  });
+});
+
+//======================
+//    iPad 4
+//======================
+
 router.get('/remont-ipad4', function (req, res, next) {
   req.db['ipad4'].find().sort({'idx': 1}, function (err, docs) {
     if (err) throw err;
@@ -48,6 +114,37 @@ router.get('/remont-ipad4', function (req, res, next) {
     });
   });
 });
+
+router.get('/remont-ipad4/zamena-batarei', function (req, res, next) {
+  res.render('pages/ipads/ipad4/zamena-batarei', {
+    title: "Замена батареи (аккумулятора) ipad 4 в Киеве | Service-iPhone",
+    description: "ipad быстро разряжается? Или не работает батарея? В Service-ipad быстро найдут причину неполадки и сделают качественный ремонт",
+    keywords: "Замена батареи ipad 4, Киев, ремонт аккумулятора ipad 4, замена батареи айпад 4",
+    menuActive: 'ipad'
+  });
+});
+
+router.get('/remont-ipad4/zamena-ekrana', function (req, res, next) {
+  res.render('pages/ipads/ipad4/zamena-ekrana', {
+    title: "Замена экрана (дисплея) ipad 4 в Киеве | Service-iPhone",
+    description: "Качественная замена экрана (дисплея) на ipad 4 в Киеве. Цены от 400 грн. Время на ремонт от 40 минут.",
+    keywords: "Замена дисплея ipad 4, замена экрана ipad 4, Киев, замена дисплея айпад 4, экрана",
+    menuActive: 'ipad'
+  });
+});
+
+router.get('/remont-ipad4/zamena-stekla', function (req, res, next) {
+  res.render('pages/ipads/ipad4/zamena-stekla', {
+    title: "Замена стекла (тачскрина) ipad 4 в Киеве | Service-ipad",
+    description: "Качественная замена стекла (дисплея) на ipad 4 в Киеве. от 400 грн. Ремонт от 40 минут.",
+    keywords: "Замена стекла ipad 4, замена экрана ipad 4, Киев, замена дисплея ipad 4, экрана",
+    menuActive: 'ipad'
+  });
+});
+
+//======================
+//    iPad air
+//======================
 
 router.get('/remont-ipad-air', function (req, res, next) {
   req.db['ipadair'].find().sort({'idx': 1}, function (err, docs) {
@@ -62,6 +159,37 @@ router.get('/remont-ipad-air', function (req, res, next) {
   });
 });
 
+router.get('/remont-ipad-air/zamena-batarei', function (req, res, next) {
+  res.render('pages/ipads/ipadair/zamena-batarei', {
+    title: "Замена батареи (аккумулятора) ipad air в Киеве | Service-iPhone",
+    description: "ipad быстро разряжается? Или не работает батарея? В Service-ipad быстро найдут причину неполадки и сделают качественный ремонт",
+    keywords: "Замена батареи ipad air, Киев, ремонт аккумулятора ipad air, замена батареи айпад air",
+    menuActive: 'ipad'
+  });
+});
+
+router.get('/remont-ipad-air/zamena-ekrana', function (req, res, next) {
+  res.render('pages/ipads/ipadair/zamena-ekrana', {
+    title: "Замена экрана (дисплея) ipad air в Киеве | Service-iPhone",
+    description: "Качественная замена экрана (дисплея) на ipad air в Киеве. Цены от 200 грн. Время на ремонт от 20 минут.",
+    keywords: "Замена дисплея ipad air, замена экрана ipad air, Киев, замена дисплея айпад air, экрана",
+    menuActive: 'ipad'
+  });
+});
+
+router.get('/remont-ipad-air/zamena-stekla', function (req, res, next) {
+  res.render('pages/ipads/ipadair/zamena-stekla', {
+    title: "Замена стекла (тачскрина) ipad air в Киеве | Service-ipad",
+    description: "Качественная замена стекла (дисплея) на ipad air в Киеве. от 200 грн. Ремонт от 20 минут.",
+    keywords: "Замена стекла ipad air, замена экрана ipad air, Киев, замена дисплея ipad air, экрана",
+    menuActive: 'ipad'
+  });
+});
+
+//======================
+//    iPad air 2
+//======================
+
 router.get('/remont-ipad-air2', function (req, res, next) {
   req.db['ipadair2'].find().sort({'idx': 1}, function (err, docs) {
     if (err) throw err;
@@ -75,6 +203,37 @@ router.get('/remont-ipad-air2', function (req, res, next) {
   });
 });
 
+router.get('/remont-ipad-air2/zamena-batarei', function (req, res, next) {
+  res.render('pages/ipads/ipadair2/zamena-batarei', {
+    title: "Замена батареи (аккумулятора) ipad air 2 в Киеве | Service-iPhone",
+    description: "ipad быстро разряжается? Или не работает батарея? В Service-ipad быстро найдут причину неполадки и сделают качественный ремонт",
+    keywords: "Замена батареи ipad air 2, Киев, ремонт аккумулятора ipad air 2, замена батареи айпад air 2",
+    menuActive: 'ipad'
+  });
+});
+
+router.get('/remont-ipad-air2/zamena-ekrana', function (req, res, next) {
+  res.render('pages/ipads/ipadair 2/zamena-ekrana', {
+    title: "Замена экрана (дисплея) ipad air 2 в Киеве | Service-iPhone",
+    description: "Качественная замена экрана (дисплея) на ipad air 2 в Киеве. Цены от 200 грн. Время на ремонт от 20 минут.",
+    keywords: "Замена дисплея ipad air 2, замена экрана ipad air 2, Киев, замена дисплея айпад air 2, экрана",
+    menuActive: 'ipad'
+  });
+});
+
+router.get('/remont-ipad-air2/zamena-stekla', function (req, res, next) {
+  res.render('pages/ipads/ipadair 2/zamena-stekla', {
+    title: "Замена стекла (тачскрина) ipad air 2 в Киеве | Service-ipad",
+    description: "Качественная замена стекла (дисплея) на ipad air 2 в Киеве. от 200 грн. Ремонт от 20 минут.",
+    keywords: "Замена стекла ipad air 2, замена экрана ipad air 2, Киев, замена дисплея ipad air 2, экрана",
+    menuActive: 'ipad'
+  });
+});
+
+//======================
+//    iPad mini
+//======================
+
 router.get('/remont-ipad-mini', function (req, res, next) {
   req.db['ipadmini'].find().sort({'idx': 1}, function (err, docs) {
     if (err) throw err;
@@ -87,6 +246,38 @@ router.get('/remont-ipad-mini', function (req, res, next) {
     });
   });
 });
+
+router.get('/remont-ipad-mini/zamena-batarei', function (req, res, next) {
+  res.render('pages/ipads/ipadmini/zamena-batarei', {
+    title: "Замена батареи (аккумулятора) ipad mini в Киеве | Service-iPhone",
+    description: "ipad быстро разряжается? Или не работает батарея? В Service-ipad быстро найдут причину неполадки и сделают качественный ремонт",
+    keywords: "Замена батареи ipad mini, Киев, ремонт аккумулятора ipad mini, замена батареи айпад mini",
+    menuActive: 'ipad'
+  });
+});
+
+router.get('/remont-ipad-mini/zamena-ekrana', function (req, res, next) {
+  res.render('pages/ipads/ipadmini/zamena-ekrana', {
+    title: "Замена экрана (дисплея) ipad mini в Киеве | Service-iPhone",
+    description: "Качественная замена экрана (дисплея) на ipad mini в Киеве. Цены от 200 грн. Время на ремонт от 20 минут.",
+    keywords: "Замена дисплея ipad mini, замена экрана ipad mini, Киев, замена дисплея айпад mini, экрана",
+    menuActive: 'ipad'
+  });
+});
+
+router.get('/remont-ipad-mini/zamena-stekla', function (req, res, next) {
+  res.render('pages/ipads/ipadmini/zamena-stekla', {
+    title: "Замена стекла (тачскрина) ipad mini в Киеве | Service-ipad",
+    description: "Качественная замена стекла (дисплея) на ipad mini в Киеве. от 200 грн. Ремонт от 20 минут.",
+    keywords: "Замена стекла ipad mini, замена экрана ipad mini, Киев, замена дисплея ipad mini, экрана",
+    menuActive: 'ipad'
+  });
+});
+
+//======================
+//    iPad mini 2
+//======================
+
 router.get('/remont-ipad-mini2', function (req, res, next) {
   req.db['ipadmini2'].find().sort({'idx': 1}, function (err, docs) {
     if (err) throw err;
@@ -100,6 +291,37 @@ router.get('/remont-ipad-mini2', function (req, res, next) {
   });
 });
 
+router.get('/remont-ipad-mini2/zamena-batarei', function (req, res, next) {
+  res.render('pages/ipads/ipadmini2/zamena-batarei', {
+    title: "Замена батареи (аккумулятора) ipad mini 2 в Киеве | Service-iPhone",
+    description: "ipad быстро разряжается? Или не работает батарея? В Service-ipad быстро найдут причину неполадки и сделают качественный ремонт",
+    keywords: "Замена батареи ipad mini 2, Киев, ремонт аккумулятора ipad mini2, замена батареи айпад mini 2",
+    menuActive: 'ipad'
+  });
+});
+
+router.get('/remont-ipad-mini2/zamena-ekrana', function (req, res, next) {
+  res.render('pages/ipads/ipadmini2/zamena-ekrana', {
+    title: "Замена экрана (дисплея) ipad mini2 в Киеве | Service-iPhone",
+    description: "Качественная замена экрана (дисплея) на ipad mini 2 в Киеве. Цены от 200 грн. Время на ремонт от 20 минут.",
+    keywords: "Замена дисплея ipad mini 2, замена экрана ipad mini 2, Киев, замена дисплея айпад mini2, экрана",
+    menuActive: 'ipad'
+  });
+});
+
+router.get('/remont-ipad-mini2/zamena-stekla', function (req, res, next) {
+  res.render('pages/ipads/ipadmini2/zamena-stekla', {
+    title: "Замена стекла (тачскрина) ipad mini2 в Киеве | Service-ipad",
+    description: "Качественная замена стекла (дисплея) на ipad mini 2 в Киеве. от 200 грн. Ремонт от 20 минут.",
+    keywords: "Замена стекла ipad mini 2, замена экрана ipad mini2, Киев, замена дисплея ipad mini2, экрана",
+    menuActive: 'ipad'
+  });
+});
+
+//======================
+//    iPad mini 3
+//======================
+
 router.get('/remont-ipad-mini3', function (req, res, next) {
   req.db['ipadmini3'].find().sort({'idx': 1}, function (err, docs) {
     if (err) throw err;
@@ -110,6 +332,33 @@ router.get('/remont-ipad-mini3', function (req, res, next) {
       menuActive: 'ipad',
       pricesTable: docs
     });
+  });
+});
+
+router.get('/remont-ipad-mini3/zamena-batarei', function (req, res, next) {
+  res.render('pages/ipads/ipadmini3/zamena-batarei', {
+    title: "Замена батареи (аккумулятора) ipad mini3 в Киеве | Service-iPhone",
+    description: "ipad быстро разряжается? Или не работает батарея? В Service-ipad быстро найдут причину неполадки и сделают качественный ремонт",
+    keywords: "Замена батареи ipad mini 3, Киев, ремонт аккумулятора ipad mini 3, замена батареи айпад mini 3",
+    menuActive: 'ipad'
+  });
+});
+
+router.get('/remont-ipad-mini3/zamena-ekrana', function (req, res, next) {
+  res.render('pages/ipads/ipadmini3/zamena-ekrana', {
+    title: "Замена экрана (дисплея) ipad mini3 в Киеве | Service-iPhone",
+    description: "Качественная замена экрана (дисплея) на ipad mini 3 в Киеве. Цены от 200 грн. Время на ремонт от 20 минут.",
+    keywords: "Замена дисплея ipad mini 3, замена экрана ipad mini 3, Киев, замена дисплея айпад mini 3, экрана",
+    menuActive: 'ipad'
+  });
+});
+
+router.get('/remont-ipad-mini3/zamena-stekla', function (req, res, next) {
+  res.render('pages/ipads/ipadmini3/zamena-stekla', {
+    title: "Замена стекла (тачскрина) ipad mini 3 в Киеве | Service-ipad",
+    description: "Качественная замена стекла (дисплея) на ipad mini3 в Киеве. от 200 грн. Ремонт от 20 минут.",
+    keywords: "Замена стекла ipad mini 3, замена экрана ipad mini 3, Киев, замена дисплея ipad mini 3, экрана",
+    menuActive: 'ipad'
   });
 });
 
