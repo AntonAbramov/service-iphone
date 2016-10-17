f8e5016ff06d48cd778f57db9d60fcb934dcef4f
antonabramov <anton@juniqe.com>
2016-10-17 20:38:20 +0200

    dev and prod versions

---
+++ frontend/assets/sources/jivo.js
@@ -0,0 +1,39 @@
+if (typeof window.jivo_magic_var == "undefined"){var jivo_magic_var = 123321;
+  window.jivo_config={"widget_id":"L7V0Pvk7ry","site_id":283316,"widget_color":"#383d45","widget_font_color":"light","widget_padding":"100","widget_height":"33","widget_orientation":"bottom","font_size":"14","font_family":"Arial","font_type":"normal","locale":"ru_RU","show_rate_form":0,"hide_ad":1,"secure":0,"contacts_ask":0,"style_string":"jivo_shadow jivo_rounded_corners jivo_gradient jivo_3d_effect jivo_border jivo_3px_border","chat_mode":0?"online":"offline","options":1016,"hide_offline":0,"vox_login":"jivosite@chat.jivosite.voximplant.com","avatar_url":"\/\/s3-eu-west-1.amazonaws.com\/jivo-userdata","online_widget_label":"\u041d\u0430\u043f\u0438\u0448\u0438\u0442\u0435 \u043d\u0430\u043c, \u043c\u044b \u043e\u043d\u043b\u0430\u0439\u043d!","offline_widget_label":"\u041e\u0442\u043f\u0440\u0430\u0432\u044c\u0442\u0435 \u043d\u0430\u043c \u0441\u043e\u043e\u0431\u0449\u0435\u043d\u0438\u0435!","offline_form_text":"\u041f\u043e\u0436\u0430\u043b\u0443\u0439\u0441\u0442\u0430, \u0432\u0432\u0435\u0434\u0438\u0442\u0435 \u0432\u0430\u0448\u0435 \u0441\u043e\u043e\u0431\u0449\u0435\u043d\u0438\u0435 \u0438 \u043a\u043e\u043d\u0442\u0430\u043a\u0442\u044b \u0432 \u044d\u0442\u043e\u0439 \u0444\u043e\u0440\u043c\u0435, \u0438 \u043c\u044b \u043e\u0431\u044f\u0437\u0430\u0442\u0435\u043b\u044c\u043d\u043e \u0441 \u0432\u0430\u043c\u0438 \u0441\u0432\u044f\u0436\u0435\u043c\u0441\u044f!","custom_langpack":{"lblAgentDisconnected":"\u0410\u043b\u0435\u043a\u0441\u0430\u043d\u0434\u0440 \u043f\u043e\u043a\u0438\u043d\u0443\u043b \u0447\u0430\u0442.","lblChatIsOnline":"\u041d\u0430\u043f\u0438\u0448\u0438\u0442\u0435 \u043d\u0430\u043c, \u043c\u044b \u043e\u043d\u043b\u0430\u0439\u043d!","lblConnectionLost":"\u041e\u0439 \u043a\u0442\u043e \u0442\u043e \u043f\u0435\u0440\u0435\u043a\u0443\u0441\u0438\u043b \u043a\u0430\u0431\u0435\u043b\u044c. \u0412\u043e\u0441\u0441\u0442\u0430\u043d\u0430\u0432\u043b\u0438\u0432\u0430\u0435\u043c...","lblEvaluatePlease":"\u0421\u043a\u0430\u0436\u0438\u0442\u0435, \u0447\u0442\u043e \u0432\u044b \u0434\u0443\u043c\u0430\u0435\u0442\u0435 \u043e \u043d\u0430\u0441?","lblNegative":"\u0423\u0436\u0430\u0441\u043d\u043e","lblNoOperators":"\u041a \u0441\u043e\u0436\u0430\u043b\u0435\u043d\u0438\u044e, \u0410\u043b\u0435\u043a\u0441\u0430\u043d\u0434\u0440 \u0441\u0435\u0439\u0447\u0430\u0441 \u0437\u0430\u043d\u044f\u0442!","lblPencilHint":"\u0410\u043b\u0435\u043a\u0441\u0430\u043d\u0434\u0440 \u043d\u0430\u0431\u0438\u0440\u0430\u0435\u0442 \u0441\u043e\u043e\u0431\u0449\u0435\u043d\u0438\u0435","lblPositive":"\u041f\u043e\u0439\u0434\u0435\u0442","lblThankYouMessage":"\u0421\u043f\u0430\u0441\u0438\u0431\u043e \u0437\u0430 \u0432\u0430\u0448\u0435 \u0441\u043e\u043e\u0431\u0449\u0435\u043d\u0438\u0435! \u041c\u044b\u00a0\u043e\u0431\u044f\u0437\u0430\u0442\u0435\u043b\u044c\u043d\u043e \u0441\u0432\u044f\u0436\u0435\u043c\u0441\u044f \u0441\u00a0\u0412\u0430\u043c\u0438 \u0432 \u0431\u043b\u0438\u0436\u0430\u0439\u0448\u0435\u0435 \u0432\u0440\u0435\u043c\u044f"},"base_url":"\/\/code.jivosite.com","static_host":"cdn.jivosite.com","comet":{"host":"node21.jivosite.com"},"rules":[{"name":"Proactive Invitation on Every Page","enabled":true,"type":"all","conditions":[{"condition":"online","value":true},{"condition":"time_on_page","comparator":"greater","value":10},{"condition":"time_on_site","comparator":"greater","value":20},{"condition":"time_after_close","comparator":"greater","value":300},{"condition":"time_after_invitation","comparator":"greater","value":60}],"commands":[{"command":"proactive","params":{"message":"\u0414\u043e\u0431\u0440\u044b\u0439 \u0434\u0435\u043d\u044c,  \u0447\u0435\u043c \u044f \u043c\u043e\u0433\u0443 \u043f\u043e\u043c\u043e\u0447\u044c?"}}]},{"name":"Lead Collection When Agents are Offline","enabled":true,"type":"all","conditions":[{"condition":"online","value":false},{"condition":"time_on_page","comparator":"greater","value":10},{"condition":"time_on_site","comparator":"greater","value":20},{"condition":"time_after_close","comparator":"greater","value":300},{"condition":"time_after_invitation","comparator":"greater","value":60}],"commands":[{"command":"open_offline","params":{"title":"\u0417\u0430\u0434\u0430\u0439\u0442\u0435 \u0432\u0430\u0448 \u0432\u043e\u043f\u0440\u043e\u0441.","message":"\u041e\u0441\u0442\u0430\u0432\u044c\u0442\u0435 \u0412\u0430\u0448\u0435 \u0441\u043e\u043e\u0431\u0449\u0435\u043d\u0438\u0435 \u0432 \u0444\u043e\u0440\u043c\u0435 \u043d\u0438\u0436\u0435, \u0438 \u043c\u044b \u043f\u043e\u043b\u0443\u0447\u0438\u043c \u0435\u0433\u043e \u043f\u043e \u044d\u043b\u0435\u043a\u0442\u0440\u043e\u043d\u043d\u043e\u0439 \u043f\u043e\u0447\u0442\u0435!"}}]},{"name":"Retaining Message if Agent is not Answering","enabled":true,"type":"all","conditions":[{"condition":"online","value":true},{"condition":"time_after_first_message","comparator":"greater","value":60}],"commands":[{"command":"system_message","params":{"message":"\u041e\u0436\u0438\u0434\u0430\u0439\u0442\u0435 \u043f\u043e\u0436\u0430\u043b\u0443\u0439\u0441\u0442\u0430. \u0412\u0441\u0435 \u043a\u043e\u043d\u0441\u0443\u043b\u044c\u0442\u0430\u043d\u0442\u044b \u0441\u0435\u0439\u0447\u0430\u0441 \u0437\u0430\u043d\u044f\u0442\u044b, \u043d\u043e \u0443\u0436\u0435 \u0432 \u0431\u043b\u0438\u0436\u0430\u0439\u0448\u0435\u0435 \u0432\u0440\u0435\u043c\u044f \u043a\u0442\u043e \u043d\u0438\u0431\u0443\u0434\u044c \u043e\u0441\u0432\u043e\u0431\u043e\u0434\u0438\u0442\u044c\u0441\u044f \u0438 \u043f\u043e\u043c\u043e\u0436\u0435\u0442 \u0432\u0430\u043c!"}}]}],"typing_insight":1,"contacts_settings":{"name":{"show":true,"required":false},"email":{"show":true,"required":false},"phone":{"show":true,"required":false}}};
+  (function(){
+    var RETRIES_LIMIT = 3, retriesCount = 0, timerId;
+    var isMobile = /iPhone|iPad|iPod|Android|Windows Phone/i.test(navigator.userAgent);
+    var timeoutTime = isMobile? 8000 : 6000;
+    var onerrorCb = function(type) {
+      clearTimeout(timerId);
+      if (++retriesCount >= RETRIES_LIMIT) {return;}
+      try {
+        localStorage.setItem("jv_store_cdn_resolve_time", (new Date().getTime() + 120000).toString());
+        localStorage.setItem("jv_store_cdn_unavailable", type);
+      } catch (e) {}
+      jivoLoad();
+    };
+    var getUrl = function() {
+      var timestamp = null;
+      try {
+        timestamp = parseInt(localStorage.getItem("jv_store_cdn_resolve_time"), 10);
+      } catch (e) {}
+      var cdnUnavailable = timestamp && timestamp >= (new Date()).getTime();
+      return (cdnUnavailable? window.jivo_config.base_url : "//cdn.jivosite.com") + "/js/iframe_loader.js?rand=1476356953";
+    };
+    function jivoLoad() {
+      var sc = document.getElementsByTagName("script");
+      var iA = sc[0];
+      var se = document.createElement("script");
+      se.type = "text/javascript"; se.async = true; se.charset="UTF-8";
+      se.onerror = function(){ onerrorCb("error")};
+      iA.parentNode.insertBefore(se, iA).src = getUrl();
+      timerId = setTimeout(function () {
+        if (typeof window.jivo_init === "undefined") {
+          iA.parentNode.removeChild(se);
+          onerrorCb("timeout");
+        }
+      }, timeoutTime);
+    }
+    jivoLoad();
+  })();};
