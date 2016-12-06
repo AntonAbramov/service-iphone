ed4c5dc0308f8cb4984b8ee4678112c9dfe156e8
antonabramov <anton@juniqe.com>
2016-12-06 23:32:04 +0100

    remove comments

--- routes/feedback/index.js
+++ routes/feedback/index.js
@@ -15,8 +15,7 @@ router.get('/', function (req, res, next) {
     title: "Отзывы о работе сервиса Apple | Service-Iphone",
     description: "Отзывы о работе сервисного центра 'Service-iPhone' Киева по ремонту техники Apple.",
     keywords: "отзывы service iphone, service-iphone, киев",
-    menuActive: 'feedback',
-    comments: doc
+    menuActive: 'feedback'
   });
   //var collection = [];
   //console.log('just get on feedback');
