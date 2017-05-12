97fa9e5863f6827e20fe1af62e12cabc4159c736
antonabramov <anton@juniqe.com>
2017-05-12 09:33:27 +0200

    update files

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,10 +1,16 @@
 import domHelper from './helpers/domHelper';
 import Nav from './modules/navigation';
 import Courier from './modules/courier';
-//import Feedback from './pages/feedback';
 import Landing from './pages/landing';
-Nav.init();
-Courier.init();
+
+import slider from './slider';
+//Courier.init();
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+  Nav.init();
+  slider.init();
+})
 
 if (document.getElementById('stepsforform')) {
   Landing.init();
